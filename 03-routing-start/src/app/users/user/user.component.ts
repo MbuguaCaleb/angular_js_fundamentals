da7d50e5676892ba18753d1e1f042215ccd7c8af
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-04-17 10:48:07 +0300

    route-observables

--- 03-routing-start/src/app/users/user/user.component.ts
+++ 03-routing-start/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,11 +13,22 @@ export class UserComponent implements OnInit {
   //Activated Route is very helpful in fetching the route parameters
   constructor(private route:ActivatedRoute) { }
 
+  //snapshot only reacts to the first instance of the loaded component.
+  //however for subsequent requests it will change the route and not the data
+  //we have to use observable for the subsequent routes which then now will
+  //load the data asynchronously
+  //this component data is as well updated and not only the route
+
   ngOnInit() {
     this.user={
       id: this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name']
-    }
+    };
+    this.route.params.subscribe(
+      (params:Params)=>{
+        this.user.id=params['id'];
+        this.user.name=params['name'];
+      })
   }
 
 }
