0c16a5a9f8483ebe82735f8b4b939b91262f632b
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-04-17 21:58:55 +0300

    adding-observables-to-my-route-parameters

--- 03-routing-start/src/app/users/user/user.component.ts
+++ 03-routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit,OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription:Subscription;
 
   //Activated Route is a Javascript Object with very useful properties
   //Activated Route is very helpful in fetching the route parameters
@@ -24,11 +26,17 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name']
     };
-    this.route.params.subscribe(
+
+    //Observable will listen to whereever there is a change in my Route
+    this.paramsSubscription= this.route.params.subscribe(
       (params:Params)=>{
         this.user.id=params['id'];
         this.user.name=params['name'];
       })
   }
 
+  //Its good to destroy Observables so as not to Maintain their state
+ ngOnDestroy(){
+   this.paramsSubscription.unsubscribe();
+ }
 }
