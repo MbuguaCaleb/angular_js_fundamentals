46c2f88012b0c6d07ac8074510c1d814478895fa
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-10-11 08:55:18 +0300

    child-routing

--- 03-routing-start/src/app/servers/server/server.component.ts
+++ 03-routing-start/src/app/servers/server/server.component.ts
@@ -17,6 +17,7 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    //+ is like parseInt in javascript
     const id = +this.route.snapshot.params["id"];
     this.server = this.serversService.getServer(id);
 
